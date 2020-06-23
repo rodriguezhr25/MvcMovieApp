29a4bb26ab047e5cd257abe9d569a14d7860224f
Hectr Rodriguez <rodriguezhr25@gmail.com>
2020-06-23 00:49:48 -0400

    Update

---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Home Teachers",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Comedy",
+                        Price = 7.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Other Side of Heaven ",
+                        ReleaseDate = DateTime.Parse("2002-3-13"),
+                        Genre = "Romantic Drama",
+                        Price = 8.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Resoration",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Documental",
+                        Price = 9.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Charly",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Romantic Comedy",
+                        Price = 3.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MvcMovie.Data;
+using MvcMovie.Models;
 
 namespace MvcMovie
 {
@@ -13,7 +15,25 @@ namespace MvcMovie
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
